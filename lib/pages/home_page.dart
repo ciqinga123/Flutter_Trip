c8f04d79fc3306481f7a9b8743d794957d0417f7
张冠军 <zhangguanjun@mskj.com>
2020-01-09 17:05:59 +0800

    首页轮播图添加

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -8,12 +9,34 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  List imgUrls = [
+    'https://tva1.sinaimg.cn/large/006y8mN6ly1g912qmb13sj30ii0gd756.jpg',
+    'https://tva1.sinaimg.cn/large/006y8mN6ly1g90zdenxfkj31030id78c.jpg',
+    'https://tva1.sinaimg.cn/large/006y8mN6ly1g90xdnneouj30mx07m0tk.jpg'
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("首页"),
-      )
-    );
+        body: Center(
+      child: Column(
+        children: <Widget>[
+          Container(
+            height: 160,
+            child: Swiper(
+              itemCount: imgUrls.length,
+              autoplay: true,
+              itemBuilder: (BuildContext context, int index) {
+                return Image.network(
+                  imgUrls[index],
+                  fit: BoxFit.fill,
+                );
+              },
+            pagination: SwiperPagination(),
+            ),
+          ),
+        ],
+      ),
+    ));
   }
 }
